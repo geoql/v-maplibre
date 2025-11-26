e76d59a6881fe185955e9f363cb6fae54cc994eb
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2025-11-27 00:18:55 +0530

    docs: add plausible analytics
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,9 +1,19 @@
 export default defineNuxtConfig({
   extends: ['docus'],
+  modules: [
+    [
+      '@nuxtjs/plausible',
+      {
+        domain: 'v-maplibre.geoql.in',
+        apiHost: 'https://analytics.geoql.in',
+        autoOutboundTracking: true,
+      },
+    ],
+  ],
   site: {
     name: 'v-maplibre',
     description: 'Vue 3 components for MapLibre GL',
     url: 'https://v-maplibre.geoql.io',
   },
-  compatibilityDate: '2025-01-26',
+  compatibilityDate: '2025-07-18',
 });
