b69f7fe583e9b1475333fd6515a7547b209a21ec
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-01-14 16:07:51 +0530

    feat(mapcn-vue): add MapLayerH3Cluster component for H3 hexagon clustering
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ packages/mapcn-vue/src/registry/ui/map-deckgl-geo/MapLayerH3Cluster.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import { VLayerDeckglH3Cluster } from '@geoql/v-maplibre';
+  import type { H3ClusterLayerProps } from '@deck.gl/geo-layers';
+  import type { PickingInfo } from '@deck.gl/core';
+
+  export interface MapLayerH3ClusterProps extends Partial<H3ClusterLayerProps> {
+    id: string;
+    data: unknown[] | string;
+  }
+
+  const props = defineProps<MapLayerH3ClusterProps>();
+
+  const emit = defineEmits<{
+    click: [info: PickingInfo];
+    hover: [info: PickingInfo];
+  }>();
+</script>
+
+<template>
+  <VLayerDeckglH3Cluster
+    v-bind="props"
+    @click="(info) => emit('click', info)"
+    @hover="(info) => emit('hover', info)"
+  ></VLayerDeckglH3Cluster>
+</template>
