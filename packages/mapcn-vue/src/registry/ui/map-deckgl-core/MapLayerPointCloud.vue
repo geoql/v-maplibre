54259d2182d5a200fea23ac7f38a91c16155335a
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-01-14 16:07:01 +0530

    feat(mapcn-vue): add MapLayerPointCloud component for 3D point cloud visualization
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ packages/mapcn-vue/src/registry/ui/map-deckgl-core/MapLayerPointCloud.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import { VLayerDeckglPointCloud } from '@geoql/v-maplibre';
+  import type { PointCloudLayerProps } from '@deck.gl/layers';
+  import type { PickingInfo } from '@deck.gl/core';
+
+  export interface MapLayerPointCloudProps extends Partial<PointCloudLayerProps> {
+    id: string;
+    data: unknown[] | string;
+  }
+
+  const props = defineProps<MapLayerPointCloudProps>();
+
+  const emit = defineEmits<{
+    click: [info: PickingInfo];
+    hover: [info: PickingInfo];
+  }>();
+</script>
+
+<template>
+  <VLayerDeckglPointCloud
+    v-bind="props"
+    @click="(info) => emit('click', info)"
+    @hover="(info) => emit('hover', info)"
+  ></VLayerDeckglPointCloud>
+</template>
