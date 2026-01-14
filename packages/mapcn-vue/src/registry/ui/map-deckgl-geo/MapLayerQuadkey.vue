e41d179cc4a127b8fc3afc81ca2d85afa016b7ea
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-01-14 16:07:43 +0530

    feat(mapcn-vue): add MapLayerQuadkey component for Quadkey tile visualization
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ packages/mapcn-vue/src/registry/ui/map-deckgl-geo/MapLayerQuadkey.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import { VLayerDeckglQuadkey } from '@geoql/v-maplibre';
+  import type { QuadkeyLayerProps } from '@deck.gl/geo-layers';
+  import type { PickingInfo } from '@deck.gl/core';
+
+  export interface MapLayerQuadkeyProps extends Partial<QuadkeyLayerProps> {
+    id: string;
+    data: unknown[] | string;
+  }
+
+  const props = defineProps<MapLayerQuadkeyProps>();
+
+  const emit = defineEmits<{
+    click: [info: PickingInfo];
+    hover: [info: PickingInfo];
+  }>();
+</script>
+
+<template>
+  <VLayerDeckglQuadkey
+    v-bind="props"
+    @click="(info) => emit('click', info)"
+    @hover="(info) => emit('hover', info)"
+  ></VLayerDeckglQuadkey>
+</template>
