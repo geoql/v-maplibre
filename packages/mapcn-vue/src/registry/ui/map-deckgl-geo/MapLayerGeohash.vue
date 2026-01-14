867bdb49582dd5a4739ad5d03a0f09a9389481d6
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-01-14 16:07:36 +0530

    feat(mapcn-vue): add MapLayerGeohash component for Geohash grid visualization
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ packages/mapcn-vue/src/registry/ui/map-deckgl-geo/MapLayerGeohash.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import { VLayerDeckglGeohash } from '@geoql/v-maplibre';
+  import type { GeohashLayerProps } from '@deck.gl/geo-layers';
+  import type { PickingInfo } from '@deck.gl/core';
+
+  export interface MapLayerGeohashProps extends Partial<GeohashLayerProps> {
+    id: string;
+    data: unknown[] | string;
+  }
+
+  const props = defineProps<MapLayerGeohashProps>();
+
+  const emit = defineEmits<{
+    click: [info: PickingInfo];
+    hover: [info: PickingInfo];
+  }>();
+</script>
+
+<template>
+  <VLayerDeckglGeohash
+    v-bind="props"
+    @click="(info) => emit('click', info)"
+    @hover="(info) => emit('hover', info)"
+  ></VLayerDeckglGeohash>
+</template>
