6f227fe6868052a019ba05c6a0e7602785d9fb1a
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-01-14 16:07:21 +0530

    feat(mapcn-vue): add MapLayerWMS component for WMS tile service
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ packages/mapcn-vue/src/registry/ui/map-deckgl-geo/MapLayerWMS.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  import { VLayerDeckglWMS } from '@geoql/v-maplibre';
+  import type { PickingInfo } from '@deck.gl/core';
+
+  export interface MapLayerWMSProps {
+    id: string;
+    data: string;
+    serviceType?: 'wms' | 'template';
+    layers?: string[];
+    tileSize?: number;
+    minZoom?: number;
+    maxZoom?: number;
+    opacity?: number;
+    visible?: boolean;
+    pickable?: boolean;
+  }
+
+  const props = defineProps<MapLayerWMSProps>();
+
+  const emit = defineEmits<{
+    click: [info: PickingInfo];
+    hover: [info: PickingInfo];
+  }>();
+</script>
+
+<template>
+  <VLayerDeckglWMS
+    v-bind="props"
+    @click="(info) => emit('click', info)"
+    @hover="(info) => emit('hover', info)"
+  ></VLayerDeckglWMS>
+</template>
