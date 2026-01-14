3dbb106efe0ead8a2e5aa918a0dafd98debebac6
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-01-14 16:07:09 +0530

    feat(mapcn-vue): add MapLayerGridCell component for pre-aggregated grid cells
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ packages/mapcn-vue/src/registry/ui/map-deckgl-core/MapLayerGridCell.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import { VLayerDeckglGridCell } from '@geoql/v-maplibre';
+  import type { GridCellLayerProps } from '@deck.gl/layers';
+  import type { PickingInfo } from '@deck.gl/core';
+
+  export interface MapLayerGridCellProps extends Partial<GridCellLayerProps> {
+    id: string;
+    data: unknown[] | string;
+  }
+
+  const props = defineProps<MapLayerGridCellProps>();
+
+  const emit = defineEmits<{
+    click: [info: PickingInfo];
+    hover: [info: PickingInfo];
+  }>();
+</script>
+
+<template>
+  <VLayerDeckglGridCell
+    v-bind="props"
+    @click="(info) => emit('click', info)"
+    @hover="(info) => emit('hover', info)"
+  ></VLayerDeckglGridCell>
+</template>
