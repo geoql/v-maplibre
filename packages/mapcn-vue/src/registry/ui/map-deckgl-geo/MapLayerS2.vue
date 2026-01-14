bf7c0bbbb1cfbcb1525f30ca9fdf4f64adf7e2bb
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-01-14 16:07:28 +0530

    feat(mapcn-vue): add MapLayerS2 component for Google S2 cell visualization
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ packages/mapcn-vue/src/registry/ui/map-deckgl-geo/MapLayerS2.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import { VLayerDeckglS2 } from '@geoql/v-maplibre';
+  import type { S2LayerProps } from '@deck.gl/geo-layers';
+  import type { PickingInfo } from '@deck.gl/core';
+
+  export interface MapLayerS2Props extends Partial<S2LayerProps> {
+    id: string;
+    data: unknown[] | string;
+  }
+
+  const props = defineProps<MapLayerS2Props>();
+
+  const emit = defineEmits<{
+    click: [info: PickingInfo];
+    hover: [info: PickingInfo];
+  }>();
+</script>
+
+<template>
+  <VLayerDeckglS2
+    v-bind="props"
+    @click="(info) => emit('click', info)"
+    @hover="(info) => emit('hover', info)"
+  ></VLayerDeckglS2>
+</template>
