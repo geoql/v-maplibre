241c8fdba1cad7fbe8c3a75231ca59c40921c795
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-01-14 16:06:37 +0530

    feat(mapcn-vue): add MapLayerSolidPolygon component for extruded 3D polygons
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ packages/mapcn-vue/src/registry/ui/map-deckgl-core/MapLayerSolidPolygon.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import { VLayerDeckglSolidPolygon } from '@geoql/v-maplibre';
+  import type { SolidPolygonLayerProps } from '@deck.gl/layers';
+  import type { PickingInfo } from '@deck.gl/core';
+
+  export interface MapLayerSolidPolygonProps extends Partial<SolidPolygonLayerProps> {
+    id: string;
+    data: unknown[] | string;
+  }
+
+  const props = defineProps<MapLayerSolidPolygonProps>();
+
+  const emit = defineEmits<{
+    click: [info: PickingInfo];
+    hover: [info: PickingInfo];
+  }>();
+</script>
+
+<template>
+  <VLayerDeckglSolidPolygon
+    v-bind="props"
+    @click="(info) => emit('click', info)"
+    @hover="(info) => emit('hover', info)"
+  ></VLayerDeckglSolidPolygon>
+</template>
